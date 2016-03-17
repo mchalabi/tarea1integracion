0c1f1ac3221af5599c0d38c87a03cd0ea6479b39
mchalabi <mchalabih@gmail.com>
2016-03-17 12:34:42 -0300

    falta noticias

--- web/index.php
+++ web/index.php
@@ -22,4 +22,47 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/status', function() use($app) {
+  
+  return 'Http 201';
+});
+
+$app->get('/texto', function() use($app) {
+  
+  return 'Http 201';
+});
+
+
+$app->post('/validarfirma', function() use($app) {
+  $value1= $_REQUEST('mensaje');
+  $value2= $_REQUEST('hash');
+
+  foreach (getallheaders() as $name => $value) {
+    
+    if ($name == null){
+    	return 'Http 400';
+    }
+    if ( is_string(gettype($name)) == false){
+    	return 'Http 400';
+    } 
+  }
+
+  $sha256= hash('sha256', $value1);
+
+  if($sha256 == null){
+  	returno 'Htpp 500';
+  }
+  
+  if($sha256== $value2){
+  	$devolver= value1 . ' true';
+  }
+  else{
+    $devolver= value1 . ' false'; 
+  }
+
+  return $devolver;
+
+});
+
+
 $app->run();
