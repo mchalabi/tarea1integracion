01c787294f3640cef8443fb2e6ccccd50b4557b9
mchalabi <mchalabih@gmail.com>
2016-03-17 16:18:03 -0300

    o

--- web/index.php
+++ web/index.php
@@ -44,7 +44,7 @@ $app->post('/validarFirma', function() use($app) {
   $value2= $_REQUEST ['hash'];
   $minus= strtolower($value2);
   
-  /*foreach (getallheaders() as $name => $value) {
+  foreach (getallheaders() as $name => $value) {
     
     if ($name == null){
     	$variable1 = $app->json('Http error 400', 400);
@@ -54,7 +54,7 @@ $app->post('/validarFirma', function() use($app) {
     	$variable1 = $app->json('Http error 400', 400);
     	return  $variable1;
     } 
-  }*/
+  }
 
   $sha2561= hash('sha256', $value1);
 
