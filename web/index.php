68daa46aab1b887d99df8619d592c9f6d2a74f1b
mchalabi <mchalabih@gmail.com>
2016-03-17 12:52:45 -0300

    prueba

--- web/index.php
+++ web/index.php
@@ -37,21 +37,21 @@ $app->post('/validarfirma', function() use($app) {
   $value1= $_REQUEST('mensaje');
   $value2= $_REQUEST('hash');
 
-  foreach (getallheaders() as $name => $value) {
+ // foreach (getallheaders() as $name => $value) {
     
-    if ($name == null){
-    	return json_encode('Http 400');
-    }
-    if ( is_string(gettype($name)) == false){
-    	return json_encode('Http 400');
-    } 
-  }
+   // if ($name == null){
+   // 	return json_encode('Http 400');
+   // }
+    //if ( is_string(gettype($name)) == false){
+    //	return json_encode('Http 400');
+    //} 
+  //}
 
   $sha256= hash('sha256', $value1);
 
-  if($sha256 == null){
-  	return json_encode('Htpp 500');
-  }
+  //if($sha256 == null){
+  	//return json_encode('Htpp 500');
+  //}
   
   if($sha256== $value2){
   	$devolver= value1 . ' true';
@@ -60,7 +60,7 @@ $app->post('/validarfirma', function() use($app) {
     $devolver= value1 . ' false'; 
   }
 
-  return json_encode($devolver);
+  return $devolver;
 
 });
 
